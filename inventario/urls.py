8ea17818d28870f61dfe2357fcee243839d42341
David Aguilar <ag.david086@gmail.com>
2026-03-30 22:35:01 -0400

    formularios

--- inventario/urls.py
+++ inventario/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path 
+from django.urls import path, include 
 from . import views 
 
 urlpatterns = [
+  path('contact/<str:name>', views.contact),
+  path('categorias', views.categorias, name="categorias"),
   path(
        'demo', views.index
   )
--- inventario/views.py
+++ inventario/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from  .models import Categoria
 # Create your views here.
 
 def index(request):
   return HttpResponse("Hola Mundo")
+
+def contact(request, name):
+  return HttpResponse(f"Hola {name} bienvenido a la clase django")
+
+def categorias(request):
+  categorias = Categoria.objects.all()
+  return render(request, 'form_categorias.html', {
+    "categorias": categorias
+  })
+
+ 
